001ef14a0e990641c12a1925bc7d9e80c1466995
Justin Phan <justinphan300000@yahoo.com>
2023-04-19 18:38:32 -0400

    Initial commit

---
+++ tracing.js
@@ -0,0 +1,43 @@
+const { Resource } = require("@opentelemetry/resources");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
+const { trace } = require("@opentelemetry/api");
+//Instrumentations
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
+const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
+const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
+const { registerInstrumentations } = require("@opentelemetry/instrumentation");
+
+module.exports = (serviceName) => {
+  const jaegerExporter = new JaegerExporter({
+    serviceName: serviceName,
+    // The default Jaeger endpoint to send traces to
+    // jaeger-agent:6831, the port for the Jaeger compact thrift UDP format
+    // See https://www.jaegertracing.io/docs/getting-started/
+    endpoint: 'http://localhost:14268/api/traces'
+  });
+
+  const provider = new NodeTracerProvider({
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+    }),
+  });
+
+  provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
+
+  provider.register();
+
+  registerInstrumentations({
+    instrumentations: [
+      new HttpInstrumentation(),
+      new ExpressInstrumentation(),
+      new MongoDBInstrumentation(),
+    ],
+    tracerProvider: provider,
+  });
+
+  return trace.getTracer(serviceName);
+};
